9531f80cfb11a39693563b30d4726128230bb449
Jiatao Geng <jiataogeng7@gmail.com>
2025-01-11 11:22:29 -0500

    create tables

--- database.py
+++ database.py
@@ -4,10 +4,42 @@ from dotenv import load_dotenv, dotenv_values
 load_dotenv()
 
 # Connect to MySQL database
-conn = mysql.connector.connect(
+mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password=os.getenv("password"),
     database="student_management"
 )
-print(conn)
+mycursor = mydb.cursor()
+mycursor.execute("SHOW TABLES")
+for table in mycursor:
+    print(table)
+
+# Create tables if they do not exist already
+def createTables():
+    mycursor.execute("""
+        CREATE TABLE IF NOT EXISTS students (
+            studentID INT AUTO_INCREMENT PRIMARY KEY, 
+            name VARCHAR(255), 
+            email VARCHAR(255)
+        )
+    """)
+    mycursor.execute("""
+        CREATE TABLE IF NOT EXISTS courses (
+            courseID INT AUTO_INCREMENT PRIMARY KEY, 
+            name VARCHAR(255), 
+            description VARCHAR(255)
+        )
+    """)
+    mycursor.execute("""
+        CREATE TABLE IF NOT EXISTS enrollment (
+            enrollmentID INT AUTO_INCREMENT PRIMARY KEY, 
+            studentID INT, 
+            courseID INT, 
+            FOREIGN KEY (studentID) REFERENCES students(studentID), 
+            FOREIGN KEY (courseID) REFERENCES courses(courseID)
+        )
+    """)
+
+createTables()
+
